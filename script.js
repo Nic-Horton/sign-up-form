2a6bd5ce260b9623cb6c96c9bc0ab9a127ba9a0d
Nic-Horton <77477203+Nic-Horton@users.noreply.github.com>
2023-03-08 21:43:06 -0600

    Add validation for password and confirm password

---
+++ script.js
@@ -0,0 +1,9 @@
+function validate() {
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=confirmPW]');
+    if (confirm.value === password.value) {
+      confirm.setCustomValidity('');
+    } else {
+      confirm.setCustomValidity('Passwords do not match');
+    }
+  }
